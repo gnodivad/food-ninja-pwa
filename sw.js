3fd749bf92fc1636592e1ca17a89d613ed1561ca
David <dodwei.dev@gmail.com>
2019-06-01 12:15:38 +0800

    #10 - The 'Activate' Event

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('install', evt => {
     console.log('service worker has been installed');
 })
+
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated');
+})
