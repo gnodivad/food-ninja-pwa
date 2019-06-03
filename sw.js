8691a57485b0a23cbf8690bfae4acd26c4693f04
David <dodwei.dev@gmail.com>
2019-06-03 10:44:02 +0800

    #12 - Fetch Events

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
 self.addEventListener('install', evt => {
     console.log('service worker has been installed');
-})
+});
 
 self.addEventListener('activate', evt => {
     console.log('service worker has been activated');
-})
+});
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+});
