444c924409ee57c553f1ee19c2d5f4d6171b1859
David <dodwei.dev@gmail.com>
2019-06-04 14:31:02 +0800

    #15 - Pre-Caching Assets

--- sw.js
+++ sw.js
@@ -1,11 +1,33 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
+    // console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
+    // console.log('service worker has been activated');
 });
 
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    // console.log('fetch event', evt);
 });
+
+
