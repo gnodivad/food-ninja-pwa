2e6925605b088a948cfdbeff1ac0919879678299
David <dodwei.dev@gmail.com>
2019-06-06 15:06:11 +0800

    #17 - Cache Versioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
 const assets = [
     '/',
     '/index.html',
@@ -24,6 +24,15 @@ self.addEventListener('install', evt => {
 
 self.addEventListener('activate', evt => {
     // console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', evt => {
