70775e9e15248e6b7772572e8bda7d26da74df4e
David <dodwei.dev@gmail.com>
2019-06-07 13:17:19 +0800

    #18 - Dynamic Caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -39,7 +40,12 @@ self.addEventListener('fetch', evt => {
     // console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
     )
 });
