13cbbb2a1d4add81c18a7dae7f17e605767dd666
David <dodwei.dev@gmail.com>
2019-06-12 08:56:35 +0800

    #25 - Updating the UI

--- js/db.js
+++ js/db.js
@@ -2,7 +2,7 @@
 db.collection('recipes').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
-
+            renderRecipe(change.doc.data(), change.doc.id);
         }
 
         if (change.type === 'removed') {
