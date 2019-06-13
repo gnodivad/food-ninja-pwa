305af4273b0f2fac7c3cb85eeccc4da7caa8db0d
David <dodwei.dev@gmail.com>
2019-06-13 11:37:53 +0800

    #27 - Adding Recipes

--- js/db.js
+++ js/db.js
@@ -1,3 +1,12 @@
+db.enablePersistence()
+    .catch(err => {
+        if (err.code == 'failed-precondition') {
+            console.log("Persistence failed");
+        } else if (err.code == 'unimplemented') {
+            console.log("Persistence is not available");
+        }
+    });
+
 // real-time listener
 db.collection('recipes').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach(change => {
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v1';
+const staticCacheName = 'site-static-v3';
+const dynamicCacheName = 'site-dynamic-v2';
 const assets = [
     '/',
     '/index.html',
@@ -49,22 +49,24 @@ self.addEventListener('activate', evt => {
 });
 
 self.addEventListener('fetch', evt => {
-    // console.log('fetch event', evt);
-    // evt.respondWith(
-    //     caches.match(evt.request).then(cacheRes => {
-    //         return cacheRes || fetch(evt.request).then(fetchRes => {
-    //             return caches.open(dynamicCacheName).then(cache => {
-    //                 cache.put(evt.request.url, fetchRes.clone());
-    //                 limitCacheSize(dynamicCacheName, 15);
-    //                 return fetchRes;
-    //             })
-    //         });
-    //     }).catch(() => {
-    //         if (evt.request.url.indexOf('.html')) {
-    //             return caches.match('/pages/fallback.html');
-    //         }
-    //     })
-    // )
+    if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
+        evt.respondWith(
+            caches.match(evt.request).then(cacheRes => {
+                return cacheRes || fetch(evt.request).then(fetchRes => {
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(evt.request.url, fetchRes.clone());
+                        limitCacheSize(dynamicCacheName, 15);
+                        return fetchRes;
+                    })
+                });
+            }).catch(() => {
+                if (evt.request.url.indexOf('.html')) {
+                    return caches.match('/pages/fallback.html');
+                }
+            })
+        )
+    }
+
 });
 
 
